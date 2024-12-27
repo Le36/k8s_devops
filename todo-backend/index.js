fe1dac70ef53c969f44223befe262726edb51bc8
Le36 <48809753+Le36@users.noreply.github.com>
2024-12-27 18:24:29 +0200

    Exercise 2.08: Project v1.2

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -1,26 +1,61 @@
 const express = require('express')
+const {Pool} = require('pg')
 const app = express()
 app.use(express.json())
 
-let todos = []
-let currentId = 1
 
-app.get('/backend/todos', (req, res) => {
-    res.json(todos)
+const pool = new Pool({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD
 })
 
-app.post('/backend/todos', (req, res) => {
-    const {text} = req.body
-    const newTodo = {
-        id: currentId++,
-        text: text || ''
+const initDb = async () => {
+    // language=SQL format=false
+    pool.query(`
+      CREATE TABLE IF NOT EXISTS todos (
+        id SERIAL PRIMARY KEY,
+        text VARCHAR(255) NOT NULL
+      );
+`).catch(err => console.error('Error creating table:', err))
+}
+
+
+app.get('/backend/todos', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM todos ORDER BY id')
+        res.json(result.rows)
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Failed to fetch todos')
     }
-    todos.push(newTodo)
-    res.status(201).json(newTodo)
 })
+
+app.post('/backend/todos', async (req, res) => {
+    try {
+        const {text} = req.body
+        const result = await pool.query(
+            `INSERT INTO todos (text)
+             VALUES ($1) RETURNING *`,
+            [text]
+        )
+        res.status(201).json(result.rows[0])
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Failed to create todo')
+    }
+})
+
 
 const port = process.env.PORT || 3000
-
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Server started in port ${port}`)
+    try {
+        await initDb()
+        console.log('DB init complete')
+    } catch (err) {
+        console.error('Error during DB init:', err)
+    }
 })
